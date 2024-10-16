01377f888c3bbd721619a6bc56e2f1c5f2659a65
Navneet <navneetkumar.symfony813@webkul.in>
2024-10-16 12:49:19 +0530

    Create unopim-vite.php

---
+++ src/Config/unopim-vite.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'viters' => [
+        'shopify' => [
+            'hot_file'                 => 'shopify-vite.hot',
+            'build_directory'          => 'themes/shopify/build',
+            'package_assets_directory' => 'src/Resources/assets',
+        ],
+    ],
+];
