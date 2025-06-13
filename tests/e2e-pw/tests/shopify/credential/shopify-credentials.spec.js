a09fb0644d03d50d7fc0c70f09ec513051ba664f
pravat-senapati <pravat.senapati735@webkul.in>
2025-06-13 18:44:25 +0530

    Update shopify-credentials.spec.js

--- tests/e2e-pw/tests/shopify/credential/shopify-credentials.spec.js
+++ tests/e2e-pw/tests/shopify/credential/shopify-credentials.spec.js
@@ -99,8 +99,8 @@ test.describe.serial('Shopify Create credential Page', () => {
 
   test('Credential creation with the valid data', async ({ page }) => {
     await page.getByRole('button', { name: 'Create Credential' }).click();
-    await page.getByRole('textbox', { name: 'http://demo.myshopify.com' }).fill('http://quickstart-c2b9e6cf.myshopify.com');
-    await page.getByRole('textbox', { name: 'Admin API access token' }).fill('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');
+    await page.getByRole('textbox', { name: 'http://demo.myshopify.com' }).fill('https://quickstart-4a9be6b7.myshopify.com');
+    await page.getByRole('textbox', { name: 'Admin API access token' }).fill('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');
     await page.getByRole('button', { name: 'Save' }).click();
     await expect(page.getByRole('banner')).toBeVisible();
     await expect(page.getByText('Credential Created Success')).toBeVisible();
