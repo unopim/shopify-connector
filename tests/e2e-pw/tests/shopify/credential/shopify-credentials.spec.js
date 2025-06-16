75462705542f1c01469b790a7ce134ef79f0d869
pravat-senapati <pravat.senapati735@webkul.in>
2025-06-16 10:21:25 +0530

    Update shopify-credentials.spec.js

--- tests/e2e-pw/tests/shopify/credential/shopify-credentials.spec.js
+++ tests/e2e-pw/tests/shopify/credential/shopify-credentials.spec.js
@@ -103,7 +103,7 @@ test.describe.serial('Shopify Create credential Page', () => {
     await page.getByRole('textbox', { name: 'Admin API access token' }).fill('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');
     await page.getByRole('button', { name: 'Save' }).click();
     await expect(page.getByRole('banner')).toBeVisible();
-    await expect(page.getByText('Credential Created Success')).toBeVisible();
+    await expect(page.locator('body')).toContainText('Credential Created Success');
     await page.getByRole('link', { name: 'Back' }).click();
     await expect(page.locator('#app')).toContainText('Yes');
     await page.getByText('1 Results').click();
@@ -133,7 +133,7 @@ test.describe.serial('Shopify Create credential Page', () => {
   });
 
   test('Delete the credential', async ({ page }) => {
-    await expect(page.locator('#app')).toContainText('http://quickstart-c2b9e6cf.myshopify.com');
+    await expect(page.locator('#app')).toContainText('http://quickstart-4a9be6b7.myshopify.com');
     await expect(page.getByTitle('Delete')).toBeVisible();
     await page.getByTitle('Delete').click();
     await expect(page.getByText('Are you sure you want to')).toBeVisible();
