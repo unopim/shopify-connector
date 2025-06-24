28ba497c553b6b30c954c28123957dfe2eff8e13
Pravat Senapati <pravat.senapati735@webkul.in>
2025-06-24 17:44:29 +0530

    added Import test and update export test

--- tests/e2e-pw/tests/shopify/mapping/shopify-import.spec.js
+++ tests/e2e-pw/tests/shopify/mapping/shopify-import.spec.js
@@ -1,8 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test.use({ storageState: 'storage/auth.json' }); // Reuse login session
+test.use({ storageState: 'storage/auth.json' });
 
-// Define all mapping elements from Mapping page.
 const mappingElements = [
     { field: 'Name [title]', inputName: 'title', placeholder: 'Name' },
     { field: 'Description [descriptionHtml]', inputName: 'descriptionHtml', placeholder: 'Description' },
@@ -23,12 +22,12 @@ const mappingElements = [
     { field: 'Cost per item [cost]', inputName: 'cost', placeholder: 'Cost per item' }
 ];
 
-test.describe('UnoPim Shopify mapping tab Navigation', () => {
+test.describe('UnoPim Shopify import mapping tab Navigation', () => {
     test.beforeEach(async ({ page }) => {
         // Navigate to the Shopify Credentials Page
         await page.goto('admin/shopify/credentials');
-        await page.getByRole('link', { name: 'Export Mappings' }).click();
-        await expect(page.url()).toMatch(/\/admin\/shopify\/export\/mapping\/[0-9]+$/);
+        await page.getByRole('link', { name: 'Import Mappings' }).click();
+        await expect(page.url()).toMatch(/\/admin\/shopify\/import\/mapping\/[0-9]+$/);
     });
 
     // Playwright test to map fields
@@ -41,43 +40,32 @@ test.describe('UnoPim Shopify mapping tab Navigation', () => {
 
             const input = page.locator(`input[name="${element.inputName}"]`);
         }
-        
+
         const saveButton = page.getByRole('button', { name: 'Save' });
         await saveButton.click();
         await page.getByRole('button', { name: 'Save' }).click();
-      await expect(page.locator('#app')).toContainText('The Unit Weight field is required');
-      await expect(page.locator('#app')).toContainText('The Unit Volume field is required');
-      await expect(page.locator('#app')).toContainText('The Unit Dimension field is required');
-      await page.locator('div').filter({ hasText: /^Unit Weight$/ }).click();
-      await page.getByText('kg', { exact: true }).click();
-      await page.locator('div').filter({ hasText: /^Unit Volume$/ }).click();
-      await page.getByText('L', { exact: true }).click();
-      await page.locator('div').filter({ hasText: /^Unit Dimension$/ }).click();
-      await page.getByText('cm', { exact: true }).click();
-      await page.getByRole('button', { name: 'Save' }).click();
-      await expect(page.getByText('Mapping saved successfully')).toBeVisible();
+        await expect(page.locator('#app')).toContainText('The Choose Family field is required');
+        await page.locator('div').filter({ hasText: /^Choose Family$/ }).click();
+        await page.getByText('Default', { exact: true }).click();
+        await page.getByRole('button', { name: 'Save' }).click();
+        await expect(page.getByText('Mapping saved successfully')).toBeVisible();
 
     });
 
-    test('should navigate to Shopify mapping page and fill export mapping form', async ({ page }) => {
+    test('should navigate to Shopify Import mapping page and fill import mapping form', async ({ page }) => {
         await expect(page.getByRole('link', { name: 'General' })).toBeVisible();
         await expect(page.locator('#app')).toContainText('General');
-        await expect(page.getByRole('paragraph').filter({ hasText: 'Export Mappings' })).toBeVisible();
-        await expect(page.locator('#app')).toContainText('Export Mappings');
+        await expect(page.getByRole('paragraph').filter({ hasText: 'Import Mappings' })).toBeVisible();
+        await expect(page.locator('#app')).toContainText('Import Mappings');
         await page.getByRole('button', { name: 'Save' }).click();
-        await expect(page.getByText('Export Mapping saved successfully')).toBeVisible();
-        await expect(page.locator('#app')).toContainText('Export Mapping saved successfully');
+        await expect(page.getByText('Import Mapping saved successfully')).toBeVisible();
+        await expect(page.locator('#app')).toContainText('Import Mapping saved successfully');
         await page.locator('div').filter({ hasText: /^Name$/ }).click();
         await page.getByText('Name', { exact: true }).click();
         await page.getByText('Description', { exact: true }).click();
         await page.locator('div').filter({ hasText: /^Price$/ }).click();
-        await page.locator('#default_productType').click();
-        await page.locator('#default_productType').clear();
-        await page.locator('#default_productType').fill('unopim');
-        await page.locator('#default_productType').click();
-        await page.locator('#default_tags').click();
-        await page.locator('#default_tags').fill('shopify');
-        const mediaTypeDropdown = page.locator('#mediaType .multiselect__select');
+        await page.getByText('Price', { exact: true }).click();
+        const mediaTypeDropdown = page.locator('#type .multiselect__select');
         await mediaTypeDropdown.click();
         await page.getByText('Gallery', { exact: true }).click();
         const pLocator = page.locator('p', { hasText: 'Media Attributes' });
@@ -86,8 +74,8 @@ test.describe('UnoPim Shopify mapping tab Navigation', () => {
         const hasDisabledClass = await multiselect.evaluate(el => el.classList.contains('multiselect--disabled'));
         expect(hasDisabledClass).toBe(false);
         await page.getByRole('button', { name: 'Save' }).click();
-        await expect(page.locator('#app')).toContainText('Export Mapping saved successfully');
+        await expect(page.locator('#app')).toContainText('Import Mapping saved successfully');
         await page.getByRole('link', { name: 'Back' }).click();
-        await page.getByRole('link', { name: 'Export Mappings' }).click();
+        await page.getByRole('link', { name: 'Import Mappings' }).click();
     });
 });
